d68f3b4d7cff2e54936e292fe8112eb315d58b1d
David Larsson <davvelars@hotmail.com>
2020-06-17 13:18:39 +0200

    Update firebase.js

--- server/firebase.js
+++ server/firebase.js
@@ -1,6 +1,6 @@
 
 const admin = require('firebase-admin');
-const serviceAccount = './fbadminkey.json';
+const serviceAccount = '/server/fbadminkey.json';
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
